de587d0b49bb30a90153303bea45882eaf9b0e2c
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-09-09 15:44:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ resource "aws_ebs_volume" "v2" {
     owner = "Mike"
   }
 }
+
+
+resource "aws_ebs_volume" "v4" {
+  availability_zone = "us-west-2a"
+  size = 8
+
+  tags = {
+    owner = "Mike"
+  }
+}
